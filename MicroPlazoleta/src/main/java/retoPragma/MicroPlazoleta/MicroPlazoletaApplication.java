6951f145c307a968a6783e3baf4e129f7e5ea6fa
yeferHome <yefersonrojas16yi@gmai.com>
2025-04-03 10:12:09 -0500

    Micro servicio de plazoleta con dependencias

---
+++ MicroPlazoleta/src/main/java/retoPragma/MicroPlazoleta/MicroPlazoletaApplication.java
@@ -0,0 +1,13 @@
+package retoPragma.MicroPlazoleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroPlazoletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroPlazoletaApplication.class, args);
+	}
+
+}
---
+++ MicroPlazoleta/src/test/java/retoPragma/MicroPlazoleta/MicroPlazoletaApplicationTests.java
@@ -0,0 +1,13 @@
+package retoPragma.MicroPlazoleta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroPlazoletaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
